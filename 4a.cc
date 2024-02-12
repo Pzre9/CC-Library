87c34fc42ca43ce5d51e5b42608ebc3a64f7256d
ASusAdam <105113599+Pzre9@users.noreply.github.com>
2024-02-12 21:51:48 +0700

    Watermelon

---
+++ 4a.cc
@@ -0,0 +1,14 @@
+// made by pzrentx
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    if (n <= 2) {
+        cout << "NO";
+        return 0;
+    }
+    if (n % 2 == 0) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
